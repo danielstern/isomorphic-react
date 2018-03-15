f08c3116baaf6358d137301246dac8fe1d10d7de
Daniel Stern <daniel@danielstern.ca>
2018-03-15 07:03:00 -0400

    Fix data fetching

--- src/index.jsx
+++ src/index.jsx
@@ -65,8 +65,10 @@ store.subscribe(()=>{
 const fetchDataForLocation = location=>{
     /**
      * If the location is the standard route, fetch an undetailed list of all questions
-     */
-
+     **/
+    if (location.pathname === "/"){
+        store.dispatch({type:`REQUEST_FETCH_QUESTIONS`})
+    }
 
     /**
      * If the location is the details route, fetch details for one question
