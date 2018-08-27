aa6168e68d610aac4b388905da4631b588725c0a
Sean Jun <seanjun21@gmail.com>
2018-08-26 23:19:43 -0700

    React Redux Container Testing Demo

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,4 +1,25 @@
+import { mapStateToProps } from '../QuestionDetail';
+
 describe('The Question Detail Component', () => {
+  describe('The Container Element', () => {
+    describe('mapStateToProps', () => {
+      it('Should map the state to props correctly', () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: "Space is big"
+        };
+        const appState = {
+          questions: [sampleQuestion]
+        };
+        const ownProps = {
+          question_id: 42
+        };
+        const componentState = mapStateToProps(appState, ownProps);
+        console.log(componentState);
+        expect(componentState).toEqual(sampleQuestion);
+      });
+    });
+  });
   it('Should not regress', () => {
 
   });
